ada64ea1e82f40b06eb22a775c39c03f56e81f8b
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2024-09-19 19:04:11 +0530

    committing new project

---
+++ abc/src/abc/hello.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("From another project");
+
+	}
+
+}
