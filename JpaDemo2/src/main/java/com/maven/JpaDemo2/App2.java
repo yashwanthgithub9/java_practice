79e7db7ef0e969390aef094dbf478cbc694b418e
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2024-12-01 15:28:33 +0530

    created a new entry and persisted to DB

--- JpaDemo2/src/main/java/com/maven/JpaDemo2/App2.java
+++ JpaDemo2/src/main/java/com/maven/JpaDemo2/App2.java
@@ -7,13 +7,13 @@ import jakarta.persistence.Persistence;
 /**
  * Hello world!
  */
-public class App {
+public class App2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
         Studentinfo studentinfo = new Studentinfo();
         studentinfo.setName("Bumrah");
-        studentinfo.setRoll(11);
+        studentinfo.setRoll(4);
         studentinfo.setMarks(50);
         
         System.out.println(studentinfo);
@@ -21,7 +21,15 @@ public class App {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myJpaUnit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         
-        Studentinfo studentinfo2=entityManager.find(Studentinfo.class, 2);
-        System.out.println(studentinfo2);
+//        Studentinfo studentinfo2=entityManager.find(Studentinfo.class, 3);
+//        System.out.println(studentinfo2);
+        
+        entityManager.getTransaction().begin();
+        entityManager.persist(studentinfo); // this will just persist the info, inorder to save to DB we have to begin txn and commit it
+        entityManager.getTransaction().commit();
+        System.out.println(entityManager.find(Studentinfo.class, 4));
+        
+      // now the value is save to DB
+        
     }
 }
