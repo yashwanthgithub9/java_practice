2582137558073ad2aeb811cbe7215e499de60abd
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2024-12-02 14:55:43 +0530

    implemented setter and constructor injection

--- SpringBootDemo/src/main/java/com/springboot/SpringBootDemo/Dev.java
+++ SpringBootDemo/src/main/java/com/springboot/SpringBootDemo/Dev.java
@@ -6,14 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dev {
 	
-	@Autowired //Field Injection
+//	@Autowired //Field Injection
 	// Searches byType "Laptop" and connects it using autowired
 	private Laptop laptop;
+//	//below is Constructor injection where @Autowired annotation is not needed
+//	public Dev(Laptop laptop) {
+//		this.laptop=laptop;
+//	}
+	
+	
 	
 	public void buid()
 	{
 		laptop.compile();
 		System.out.println("\nWorking on Dev Project");
 		
+	}
+
+
+	@Autowired
+	// below is setter injection
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
 	}
 }
