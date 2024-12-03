0538c699d8207e5e1261df8ddef842fa572b3a2f
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2024-12-03 21:22:17 +0530

    basic web project with about and home page are created

---
+++ SpringBootWebDemo/src/main/java/com/springboot/SpringBootWebDemo/HomeController.java
@@ -0,0 +1,25 @@
+package com.springboot.SpringBootWebDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController // this can be used in place of @controller and we can remove @responsebody
+public class HomeController {
+	
+	@RequestMapping("/") //is @Controller is used, request mapping will search for Welcom to Home page.html file
+//	@ResponseBody// to show only content we have to use this annotations
+	public String greet() {
+		return "Welcom to Home page";
+		
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		
+		return "About is under construction";
+	}
+
+}
---
+++ SpringBootWebDemo/src/test/java/com/springboot/SpringBootWebDemo/SpringBootWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SpringBootWebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
