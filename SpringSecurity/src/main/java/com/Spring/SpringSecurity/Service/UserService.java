b26e95c62f8f509c40826371ed159c08431f87f8
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2024-12-23 11:17:57 +0530

    bcrypt part-2 : password encrypted

--- SpringSecurity/src/main/java/com/Spring/SpringSecurity/Service/UserService.java
+++ SpringSecurity/src/main/java/com/Spring/SpringSecurity/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.Spring.SpringSecurity.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.Spring.SpringSecurity.Model.Users;
@@ -11,15 +12,23 @@ public class UserService {
 	
 	@Autowired
 	private UserRepo repo;
+	
+	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+	
 
 	public Users addUser(Users user) {
 		// TODO Auto-generated method stub
-		
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		return repo.save(user); //Now one user is added into DB
 	}
 	
 //	1	yash	yash@123
 //	2	reddy	reddy@123
 //	3	yashwanth	yashwanth
+	
+//	1	yash	yash@123
+//	2	reddy	reddy@123
+//	3	purra	purra@123
+//	4	bumrah	$2a$12$8yh6DQMGa4W2b.sTL96gnuoiniul7NxGHW/0G50nBbvS/l..lM7QK
 
 }
