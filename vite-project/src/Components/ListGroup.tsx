787100f9f387c855482eaf3852f790944dce670a
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-01-17 15:16:21 +0530

    Rendering List Items

--- vite-project/src/Components/ListGroup.tsx
+++ vite-project/src/Components/ListGroup.tsx
@@ -1,20 +1,21 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  const items = ["Hyderbad", "Banglore", "Chennai", "Mumbai"];
+
   return (
     // <Fragment>
     <>
       {/* Either add Fragment or keep empty braces<></> */}
-<h1>List Group</h1> 
-{/* returning h1 and ul is not possible so combined everythin in on div and returning it*/}
-    <ol className="list-group">
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ol>
-    {/* </Fragment>  */}
+      <h1>List Group</h1>
+      {/* returning h1 and ul is not possible so combined everythin in on div and returning it*/}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+        {/* put the javascript function inside {} */}
+      </ul>
+      {/* </Fragment>  */}
     </>
   );
 }
