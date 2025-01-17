d6860674c569dbbc1436659ebfaa1cd9e5aca9c4
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-01-17 21:56:08 +0530

    Event handler

--- vite-project/src/Components/ListGroup.tsx
+++ vite-project/src/Components/ListGroup.tsx
@@ -1,9 +1,10 @@
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["Hyderbad", "Banglore", "Chennai", "Mumbai"];
-  items = ["Delhi"];
-
+  // items = ["Delhi"];
+  const handler = (event: MouseEvent) => console.log(event);
   return (
     // <Fragment>
     <>
@@ -14,7 +15,11 @@ function ListGroup() {
       {/* check for condition, if items is empty then above expression will be true then <p>will be rendered */}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handler}>
+            {" "}
+            {/* Bootstrap class to format list groups*/}
+            {item}
+          </li>
         ))}
         {/* put the javascript function inside {} */}
       </ul>
