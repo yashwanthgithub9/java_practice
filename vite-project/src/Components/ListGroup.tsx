5905904a5494f14f1fa1575608bb865ed3ad6253
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-01-17 15:04:57 +0530

    Fragments

--- vite-project/src/Components/ListGroup.tsx
+++ vite-project/src/Components/ListGroup.tsx
@@ -1,13 +1,22 @@
+import { Fragment } from "react/jsx-runtime";
 
-function ListGroup(){
-
-    return <ul className="list-group">
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>;
+function ListGroup() {
+  return (
+    // <Fragment>
+    <>
+      {/* Either add Fragment or keep empty braces<></> */}
+<h1>List Group</h1> 
+{/* returning h1 and ul is not possible so combined everythin in on div and returning it*/}
+    <ol className="list-group">
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ol>
+    {/* </Fragment>  */}
+    </>
+  );
 }
 
 export default ListGroup;
