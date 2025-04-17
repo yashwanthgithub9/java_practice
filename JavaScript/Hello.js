512f6e5ce884608aac1c87dbb37dbb6dcc27d648
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-04-17 12:55:29 +0530

    map,filter, reduce

--- JavaScript/Hello.js
+++ JavaScript/Hello.js
@@ -2,10 +2,10 @@ console.log("Hello World");
 //alert("Stop");
 
 let a = 10;
-console.log(a+5);
+console.log(a + 5);
 
-let age1= 19;
-console.log(age1>18?"Adult":"Not Adult");
+let age1 = 19;
+console.log(age1 > 18 ? "Adult" : "Not Adult");
 let iceCream = "chocolate";
 // if (iceCream === "chocolate") {
 //   alert("Yay, I love chocolate ice cream!");
@@ -20,32 +20,65 @@ if (age !== null) {
     console.log("User cancelled the prompt.");
 }*/
 
-let obj={
-    item:"pen",
-    price:15
-}
-console.log("Cost of " +obj.item+" is "+obj.price+" Rupees ");//spaces are explicitly added, price is converted to string so not highlighted in console
-console.log("Cost of",obj.item,"is",obj.price,"Rupees");//spaces are automatically added, price is still int so highlighted
-console.log(`Cost of ${obj.item} is ${obj.price} Rupees`);//spaces are expicitly added, price is converted to string so not highlighted in console
+let obj = {
+  item: "pen",
+  price: 15,
+};
+console.log("Cost of " + obj.item + " is " + obj.price + " Rupees "); //spaces are explicitly added, price is converted to string so not highlighted in console
+console.log("Cost of", obj.item, "is", obj.price, "Rupees"); //spaces are automatically added, price is still int so highlighted
+console.log(`Cost of ${obj.item} is ${obj.price} Rupees`); //spaces are expicitly added, price is converted to string so not highlighted in console
 
 // let fullName=prompt("Please enter your name:");
 // let username = "@"+fullName;
 // username+=fullName.length;
 // console.log("Userame generated based on entered name is : ",username);
 
-let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
 console.log(companies.shift());
-companies.splice(1,1,"Ola");
+companies.splice(1, 1, "Ola");
 companies.push("Amazon");
 console.log(companies);
 
-function countVowels(str){
-    let count=0;
-    for (let element of str) {
-        if (element==='a'||element==='e'||element==='i'||element==='o'||element==='u') {
-            count++
-        }
+function countVowels(str) {
+  let count = 0;
+  for (let element of str) {
+    if (
+      element === "a" ||
+      element === "e" ||
+      element === "i" ||
+      element === "o" ||
+      element === "u"||
+      element === "A" ||
+      element === "E" ||
+      element === "I" ||
+      element === "O" ||
+      element === "U"
+    ) {
+      count++;
     }
-    console.log(count);
+  }
+  console.log(count);
 }
 countVowels("Aaa");
+
+let arr=[2,345,8,4,6];
+arr.forEach((val)=>console.log(val*val));
+
+let arr1=[1,234,3,4];
+const maxnum=arr1.reduce((p,c)=>p>c?p:c);
+console.log("Maxnum is: ",maxnum);
+let newarry=arr1.filter((val)=>{return val%2===0});
+console.log(newarry);
+const init=5;
+const sum=arr1.reduce((res,cur)=>res+cur,init);
+console.log(sum);
+
+let n=prompt("Enter a number");
+let array=[];
+for(let i=1;i<=n;i++){
+    array[i-1]=i;
+}
+console.log(array);
+
+console.log(`Sum of firt ${n} digits is : `,array.reduce((res,cur)=>res+cur));
+console.log(`Factorial of ${n} is : `,array.reduce((prev,cur)=>prev*cur));
