957492a9bcca86dff848b9e91e2ed4fd2a6f8bc2
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-04-21 09:56:09 +0530

    nested callback for synchronous execution

--- JavaScript/advanced/app.js
+++ JavaScript/advanced/app.js
@@ -10,8 +10,11 @@ console.log("three");// js doesn't stop for timeout to execute, if we see consol
 //this is called asynchrounous way of execution
 console.log("four");
 
-function getData(dataId){
+function getData(dataId,getNextData){
     setTimeout(()=>{
+        if (getNextData) {
+            getNextData();
+        }
         console.log("data = ",dataId);
     },2000)
 }
@@ -22,3 +25,9 @@ getData(4); //2s timer started
 // all the timers started at same time so after 2 sec all three will log output
 // we need 6sec execution time but time we got is 2 sec
 
+getData(5,getData(6));// we cannont pass this way as getdata(2) will get executed immediatly
+getData(7,()=>{
+    getData(8,()=>{// this nested Callback will wait 2sec for (1) to execute and go for (2)
+        getData(9);// as we didn't mention any next funtoin it will stop here
+    })
+})
