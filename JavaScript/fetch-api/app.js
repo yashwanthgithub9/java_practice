a333989555f249a1aa4e7d3225cef3d099dc8a4b
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-04-22 07:51:21 +0530

    all about api

--- JavaScript/fetch-api/app.js
+++ JavaScript/fetch-api/app.js
@@ -4,25 +4,25 @@ let btn=document.querySelector("#btn");
 
 
 // Using Async await
-// const getFacts=async ()=>{ //making this arrow function async
-//     console.log("getting facts...!")
-//     let response=await fetch(url); // fetch is an asynchronous operation so putting await
-//     console.log(response); // JSON format
-//     let datatext=await response.json(); //JSON to JS object conversion
-//     console.log(datatext.data);// trying to print JS objects
-//     para.innerText=datatext.data;
-// }
-
-
-// Using Promise chains
-function getFacts(){
-    fetch(url).then((respone)=>{
-        return respone.json();
-    }).then((d)=>{
-        console.log(d.data);
-        para.innerText=d.data;
-    })
-
+const getFacts=async ()=>{ //making this arrow function async
+    console.log("getting facts...!")
+    let response=await fetch(url); // fetch is an asynchronous operation so putting await
+    console.log(response); // JSON format
+    let datatext=await response.json(); //JSON to JS object conversion
+    console.log(datatext.data);// trying to print JS objects
+    para.innerText=datatext.data;
 }
+
+
+// // Using Promise chains
+// function getFacts(){
+//     fetch(url).then((respone)=>{
+//         return respone.json();
+//     }).then((d)=>{
+//         console.log(d.data);
+//         para.innerText=d.data;
+//     })
+
+// }
 
 btn.addEventListener("click",getFacts);
