bfaed87ce08faa82da0346db6c52d09c22da2507
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-04-22 10:15:35 +0530

    updated flags for each select drop down

--- JavaScript/currency-converter/currency.js
+++ JavaScript/currency-converter/currency.js
@@ -2,6 +2,30 @@ const base_url="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1
 
 const dropdownS = document.querySelectorAll(".dropdown select");
 
-for (code in countryList) {
-    console.log(code,countryList[code]);
+// for (code in countryList) {
+//     console.log(code,countryList[code]);
+// }
+
+// We are now linking all country code to dropdowns
+for (let select of dropdownS) { // 2 selects in dropdowns "From" and "TO"
+    for (let currCode in countryList) {// for each select we are assigning country code
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);//target is to which the flag has to change
+    });
+}
+
+const updateFlag=(el)=>{
+    console.log(el);
+    let currCode=el.value;
+    console.log(currCode);
+    let countryCode=countryList[currCode];
+    console.log(countryCode);
+    let newSrc= `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img=el.parentElement.querySelector("img");//.parentElement and .queryselector are not suggesting we had to type completely
+    img.src=newSrc;
 }
