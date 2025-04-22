72479e59f2a3783df0802d8d9f77f6661a00d9f3
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-04-22 09:55:08 +0530

    structure-style of currency converter

---
+++ JavaScript/currency-converter/currency.js
@@ -0,0 +1,7 @@
+const base_url="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+
+const dropdownS = document.querySelectorAll(".dropdown select");
+
+for (code in countryList) {
+    console.log(code,countryList[code]);
+}
