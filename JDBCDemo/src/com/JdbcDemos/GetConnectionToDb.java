85066b7253555383f4075d6cc9a2e19a896332f3
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-09-04 19:02:00 +0530

    Implemented dynamic input to scheduled matches

--- JDBCDemo/src/com/JdbcDemos/GetConnectionToDb.java
+++ JDBCDemo/src/com/JdbcDemos/GetConnectionToDb.java
@@ -10,14 +10,14 @@ public class GetConnectionToDb {
 		// TODO Auto-generated method stub
 		
 //		String url ="jdbc:mysql://localhost:3306/students";//database name is student
-		String url ="jdbc:mysql://localhost:3306/cricket";//datacricketbase name is student
+		String url ="jdbc:mysql://localhost:3306/cricket";//database name is student
 
 		String username ="root";
 		String password = "root";
 		// for the mysql connector that i we downloaded, connector is not com.mysql.jdbc.Driver
 		// we are changing the connector to com.mysql.cj.jdbc.Driver
 		//Class.forName("com.mysql.cj.jdbc.Driver");//Load driver
-		Class.forName("com.mysql.jdbc.Driver");//Load driver
+		Class.forName("com.mysql.cj.jdbc.Driver");//Load driver
 
 		// Checked exception, have to handle now so add throws
 		
--- JDBCDemo/src/com/JdbcDemos/GetUpdate.java
+++ JDBCDemo/src/com/JdbcDemos/GetUpdate.java
@@ -2,7 +2,7 @@ package com.JdbcDemos;
 
 import java.sql.*;
 
-import com.mysql.jdbc.PreparedStatement;
+import com.mysql.cj.jdbc.PreparedStatementWrapper;
 
 public class GetUpdate {
 
@@ -17,6 +17,20 @@ public class GetUpdate {
 		ps.setDate(2, Date.valueOf("2024-09-09"));
 		ps.setString(3, "LSG");
 //		int rs = st.executeUpdate(string);
+		int rs1= ps.executeUpdate();
+		System.out.println("Effective number of rows = "+rs1);
+		
+	}
+
+	public void update(String string, String home, String d, String opp) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		String str = string;
+		GetConnectionToDb gt = new GetConnectionToDb();
+		Connection con =gt.getCon();
+		java.sql.PreparedStatement ps = con.prepareStatement(str);
+		ps.setString(1,home);
+		ps.setDate(2, Date.valueOf(d));
+		ps.setString(3,opp);
 		int rs1= ps.executeUpdate();
 		System.out.println("Effective number of rows = "+rs1);
 		
