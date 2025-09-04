6021819e87f69e42f93e2af26e6cc7c50ac0070f
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-09-04 10:38:17 +0530

    test commit after long time

--- abc/src/abc/hello.java
+++ abc/src/abc/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("From another project");
+		System.out.println("After Along time");
 
 	}
 
