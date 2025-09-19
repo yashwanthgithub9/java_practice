ba3fdc79c3e8c742ad837fb7e3f0a68a95a68908
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-09-19 19:40:31 +0530

    param constructor

--- JavaPractice/src/com/oops/Constr.java
+++ JavaPractice/src/com/oops/Constr.java
@@ -1,10 +1,10 @@
 package com.oops;
 class Demo {
     int num;
-    Demo() {
-        this(100); // Constructor chaining
-        System.out.println("Default Constructor");
-    }
+//    Demo() {
+//        this(100); // Constructor chaining
+//        System.out.println("Default Constructor");
+//    }
     Demo(int n) {
         num = n;
         System.out.println("Parameterized Constructor");
@@ -14,7 +14,9 @@ public class Constr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	        Demo obj = new Demo();
+//	        Demo obj = new Demo(); 
+	        Demo obj1 =new Demo(0);// when obj is created it will search for default constructor, if there is 
+	        // a parameterized constructor java will not provide a default constructor
 
 	}
 
