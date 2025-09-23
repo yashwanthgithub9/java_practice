9b742f3d55744e2b568e7228c3fad743777f493e
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-09-23 13:08:18 +0530

    performed update using merge()

--- JpaDemo2/src/main/java/com/maven/JpaDemo2/App2.java
+++ JpaDemo2/src/main/java/com/maven/JpaDemo2/App2.java
@@ -1,5 +1,7 @@
 package com.maven.JpaDemo2;
 
+import org.hibernate.Remove;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,10 +13,13 @@ public class App2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        Studentinfo studentinfo = new Studentinfo();
-        studentinfo.setName("Hardik");
-//        studentinfo.setRoll(4);
-        studentinfo.setMarks(50);
+        Studentinfo studentinfo = new Studentinfo("Virat",18);
+        Studentinfo studentinfo1 = new Studentinfo("Rohit",45);        
+        Studentinfo studentinfo2 = new Studentinfo("Gill",77);        
+        Studentinfo studentinfo3 = new Studentinfo("Abhishek",23);        
+
+//        studentinfo.setName("Hardik");
+//        studentinfo.setMarks(50);
         
         System.out.println(studentinfo);
         
@@ -23,13 +28,42 @@ public class App2 {
         
 //        Studentinfo studentinfo2=entityManager.find(Studentinfo.class, 3);
 //        System.out.println(studentinfo2);
-        
+/*       
         entityManager.getTransaction().begin();
+        // this is creating rows with incremental roll but duplicate names every time application is running
+        // need to set up unique constraint on name as well;
         entityManager.persist(studentinfo); // this will just persist the info, inorder to save to DB we have to begin txn and commit it
+        entityManager.persist(studentinfo1); // this will just persist the info, inorder to save to DB we have to begin txn and commit it
+        entityManager.persist(studentinfo2); // this will just persist the info, inorder to save to DB we have to begin txn and commit it
+        entityManager.persist(studentinfo3); // this will just persist the info, inorder to save to DB we have to begin txn and commit it
+        Studentinfo s= entityManager.find(Studentinfo.class, 1);
+        s.setName("Virat Kohli");
+        
+        
         entityManager.getTransaction().commit();
-        System.out.println(entityManager.find(Studentinfo.class, 5));
+        entityManager.close();
+//        System.out.println(entityManager.find(Studentinfo.class, 5));
         
       // now the value is save to DB
+        */
+        //Merge operation
+        //1. create entity manager
+        EntityManager em= entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        //2. Find entity and detach 
+        Studentinfo studentinfo4=em.find(Studentinfo.class, 1);
+//        em.getTransaction().commit();
+        em.close();
+        // 3. Update value
+        studentinfo4.setName("Virat Kohli(Updated Name using merge)");
+        // 4. new entity manager
+        EntityManager em1=entityManagerFactory.createEntityManager();
+        em1.getTransaction().begin();
+        Studentinfo s1=em1.merge(studentinfo4);
+        em1.getTransaction().commit();
+        em1.close();
+        
+        
         
     }
 }
--- JpaDemo2/src/main/java/com/maven/JpaDemo2/Studentinfo.java
+++ JpaDemo2/src/main/java/com/maven/JpaDemo2/Studentinfo.java
@@ -1,18 +1,34 @@
 package com.maven.JpaDemo2;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "name","marks" }))
+//Above line enforces name and mark to be unique with other entries
 public class Studentinfo {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int roll;
+	@Column(name="Cricketer")
 	private String name;
+	@Column(name="Jersey")
 	private int marks;
+	
+	public Studentinfo() {
+		super();
+	}
+	public Studentinfo(String string, int i) {
+		// TODO Auto-generated constructor stub
+		this.name=string;
+		this.marks=i;
+	}
 	public int getRoll() {
 		return roll;
 	}
