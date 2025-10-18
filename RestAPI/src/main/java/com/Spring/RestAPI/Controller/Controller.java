8d5027d29aa923b2a369646c70a6327fdd365493
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-10-18 19:00:30 +0530

    Added few line for @RequestParam and @PathVariable

--- RestAPI/src/main/java/com/Spring/RestAPI/Controller/Controller.java
+++ RestAPI/src/main/java/com/Spring/RestAPI/Controller/Controller.java
@@ -1,8 +1,11 @@
 package com.Spring.RestAPI.Controller;
 
+import ch.qos.logback.core.boolex.EvaluationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -14,8 +17,8 @@ public class Controller {
 
     //POST for Creating new detail
     @PostMapping("/details")
-    public String addDetails(@RequestBody Details detail) {
-        details.add(detail);
+    public String addDetails(@RequestBody List<Details> detail) { // changed post mapping to accept list of details
+        details.addAll(detail); //addAll method to add list
         return "Data Inserted";
     }
     //GET for fetching new detail
@@ -24,7 +27,7 @@ public class Controller {
         return details;
     }
     //PUT for Update
-    @PutMapping("details/{id}")
+    @PutMapping("/details/{id}")
     public String updateDetails(@PathVariable int id, @RequestBody Details detail){
         for (Details d:details){
             if (d.getId()==id){
@@ -42,6 +45,22 @@ public class Controller {
         return "Deleted Detail";
     }
 
+    @GetMapping(value = "/details/{id}")
+    public List<Details> getUniqueDetails(@PathVariable int id) {
+        return Collections.singletonList(details.get(id));// This creates a List that contains exactly one element — the item at position id in your details list.
+    }
 
+/*    @GetMapping("/search")
+    public ResponseEntity<List<Details>> listResponseEntity(
+            @RequestParam(value = "name",defaultValue = "Yash",required = false) String name){
+            System.out.println(listResponseEntity(name).getBody());
+        return (ResponseEntity<List<Details>>) ResponseEntity.ok(details);
 
+    }*/
+    @GetMapping("/hello")
+    public String sayHello(
+            @RequestParam String name
+    ){
+        return "Hello "+name;
+    }
 }
