d12e4ec6f406d4d21abe3f6df8758603617ab6f3
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2025-10-22 11:27:48 +0530

    Initial Setup of ToDo App

---
+++ smarttodo/src/main/java/com/todoapp/smarttodo/SmarttodoApplication.java
@@ -0,0 +1,13 @@
+package com.todoapp.smarttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttodoApplication.class, args);
+	}
+
+}
