0451a4d03e125c7a5cb87a6482402f14441cea0d
yashwanthgithub9 <yashwanthreddyau98@gmail.com>
2026-01-05 19:02:06 +0530

    Added gatewaycheck filter to force use 9191 port for entry

--- MicroserviceProjects/Employee-Service/src/main/java/com/Employee_Service/Filter/GatewayCheckFilter.java
+++ MicroserviceProjects/Employee-Service/src/main/java/com/Employee_Service/Filter/GatewayCheckFilter.java
@@ -1,4 +1,27 @@
 package com.Employee_Service.Filter;
 
-public class GatewayCheckFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class GatewayCheckFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String sourceHeader = request.getHeader("X-Source-System");
+
+        if (sourceHeader==null || !sourceHeader.equals("Active-Gateway-Secured")){
+            System.out.println("ALERT: Direct Access blocked");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("Direct Access Forbidden. Please use the API Gateway.");
+            return;
+        }
+        filterChain.doFilter(request,response);
+    }
 }
